86a79266b0db3c6bcb5103f35442be30cb7a95a1
BuildTools <unconfigured@null.spigotmc.org>
2020-07-24 12:39:22 -0600

    Corrected problem 4. I had misunderstood the question.

--- python3/problem4.py
+++ python3/problem4.py
@@ -9,15 +9,14 @@ from typing import List
 class Solution:
 
     def median(self, lst: List[int]) -> float:
-        # Time complexity should not exceed O(log (n+m)), so this method only works if python has an O(1) implementation for len() on a list
-        # Fortunately, the Python wiki claims that it does: https://wiki.python.org/moin/TimeComplexity
         length = len(lst)
         mid = int(length/2)
         if length % 2:
-            return sum(lst[mid:mid+1])/2
-        else:
             return lst[mid]
+        else:
+            return (lst[mid-1] + lst[mid])/2
 
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        return (self.median(nums1) + self.median(nums2))/2
+        # Time complexity may be in excess of O(log(n+m)
+        return self.median(sorted(nums1 + nums2))
 
