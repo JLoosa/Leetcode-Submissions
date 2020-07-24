caa0dc7ecda233d24faaf4e482986ea66705eee3
BuildTools <unconfigured@null.spigotmc.org>
2020-07-24 10:51:08 -0600

    Problem 1 completed using Python 3

---
+++ python3/problem1.py
@@ -0,0 +1,21 @@
+# Leetcode Problem 1
+# Name: "Two Sum"
+# URL: https://leetcode.com/problems/two-sum/
+# Date: 2020-07-24
+class Solution:
+
+    # Solution using nested loops
+    def twoSum_A(self, nums: List[int], target: int) -> List[int]:
+        num_count = len(nums)
+        for x in range(num_count):
+            for y in range(x, num_count):
+                if nums[x] + nums[y] == target:
+                    return [x, y]
+
+    # Solution using list comprehension
+    def twoSum_B(self, nums: List[int], target: int) -> List[int]:
+        return [[x, y] for x in range(len(nums)) for y in range(x, len(nums)) if nums[x] + nums[y] == target][0]
+
+    # Method called by the site
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return self.twoSum_B(nums, target)
