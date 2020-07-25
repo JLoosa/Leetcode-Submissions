3c33d7fab3a51a53f2ed82278244354d7a36db98
BuildTools <unconfigured@null.spigotmc.org>
2020-07-25 11:00:13 -0600

    Problem 1 corrected

--- python3/problem1.py
+++ python3/problem1.py
@@ -8,18 +8,31 @@ from typing import List
 
 class Solution:
 
-    # Solution using nested loops
-    def twoSum_A(self, nums: List[int], target: int) -> List[int]:
-        num_count = len(nums)
-        for x in range(num_count):
-            for y in range(x, num_count):
-                if nums[x] + nums[y] == target:
-                    return [x, y]
-
-    # Solution using list comprehension
-    def twoSum_B(self, nums: List[int], target: int) -> List[int]:
-        return [[x, y] for x in range(len(nums)) for y in range(x, len(nums)) if nums[x] + nums[y] == target][0]
-
-    # Method called by the site
+    # Solution using a binary search approach
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return self.twoSum_B(nums, target)
+        length = len(nums)
+        # We need to keep the original as the algorithm is meant to work on unsorted data
+        sorted_list = sorted(nums)
+        for a in range(length-1):
+            s_t = target - nums[a]
+            if self.binary_search(sorted_list, s_t):
+                try:
+                    # We do the search from a+1 because we don't want to let a be used twice
+                    b = nums.index(s_t, a+1)
+                    return [a, b]
+                except ValueError:
+                    continue
+
+    # Returns true if the target is in the provided list
+    # False other wise
+    def binary_search(self, sorted_list: List[int], target: int) -> bool:
+        lo, hi = 0, len(sorted_list) -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if sorted_list[mid] == target:
+                return True
+            elif sorted_list[mid] < target:
+                lo = mid+1
+            else:
+                hi = mid-1
+        return False
